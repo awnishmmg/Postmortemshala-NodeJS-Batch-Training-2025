24f598cad633efac5c36ebe3129495d07ad0257b
awnish kumar <awnish.kumar@indiamart.com>
2025-09-26 14:30:25 +0530

    Api

--- node/18.mongoose-api/app.js
+++ node/18.mongoose-api/app.js
@@ -2,6 +2,8 @@ require('dotenv').config({quiet:true});
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const {check,body,validationResult} = require('express-validator');
+const bcrypt = require('bcrypt');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -21,31 +23,7 @@ app.get('/',function(req,res){
     })
 });
 
-app.get('/student',function(req,res){
-
-    // const student  from mongodb
-    const students = [
-        {
-            name : "A",
-            class  : "B"
-        }
-    ];
-
-    res.status(200).json({
-        code : 200,
-        status : true,
-        message : "Student Record found",
-        data : students,
-        error : [],
-    })
-});
-
-
-
-
-
-
-
+// Connection
 function connect(){
     const db = mongoose.createConnection(process.env.MONGODB_URI);
     db.on('connected',function(){
@@ -58,9 +36,163 @@ function connect(){
     return db;
 }
 
+// Rest Api : student
+const db = connect();
+const studentSchema = new mongoose.Schema({
+    _id : {
+        type : mongoose.Schema.Types.ObjectId,
+        required : true,
+        trim : true,
+        alias :  "std_id"
+    },
+    std_name  : {
+        type : mongoose.Schema.Types.String,
+        required : true,
+        trim: true,
+    },
+    std_marks : {
+        type : mongoose.Schema.Types.Number,
+        required : true,
+        min : 0,
+        max : 100,
+    },
+    std_class : {
+        type :mongoose.Schema.Types.String,
+        required : true,
+        trim : true,
+    },
+    std_email : {
+        type : mongoose.Schema.Types.String,
+        unique : true,
+        required : true,
+        trim : true,
+    },
+    std_password : {
+        type : mongoose.Schema.Types.String,
+        required : true,
+        trim : true,
+    }
+});
+
+const StudentModel = db.model('StudentModel',studentSchema,'students');
+
+//Define Validation Rules
+const ValidationRules = [   
+    //Student_name
+     body('std_name')
+    .isEmpty()
+    .withMessage('Student Name is Required')
+    .matches(/^[a-zA-Z ]+$/)
+    .withMessage('Enter the Valid Name')
+    .trim(),
+
+    //student class 
+    body('std_class')
+    .isEmpty()
+    .withMessage('class is required')
+    .trim(),
+
+    //student marks
+    body('std_marks')
+    .isEmpty()
+    .withMessage('marks are required')
+    .trim(),
+
+    //student email 
+    body('std_email')
+    .isEmpty()
+    .withMessage('Email is required')
+    .isEmail()
+    .withMessage('Enter Valid Email')
+    .trim()
+    .custom(function(value,context){
+        StudentModel.findOne({std_email : value}).then(function(data){
+            if(data){
+                throw new Error("This Email already exist,try different email");
+            }
+        }).catch(function(error){
+            throw new Error(error);
+        });
+        return true;
+    }),
+
+    body('std_password').isEmpty().withMessage('Password is Required').trim()
+
+]
+
+app.post('/student',ValidationRules, function(req,res){
+    const errors = validationResult(req);
+    if(errors.isEmpty()){
+            //save the student
+        const student = new StudentModel({
+        std_name : req.body.std_name,
+        std_class : req.body.std_class,
+        std_marks : req.body.std_marks,
+        std_email : req.body.std_email,
+        std_password : bcrypt.hash(req.body.hash,16)
+    })
+    student.save().then(function(data){
+        res.status(201).json({
+            code : 201,
+            status : true,
+            error : [],
+            message : "Student Record created",
+            data : data
+        });
+    }).catch(function(error){
+        res.status(500).json({
+            code : 500,
+            status : false,
+            error : [error],
+            message : "Something went wrong",
+            data : []
+        });
+    });
+
+            //end the student
+    }else{
+        res.status(400).json({
+            code : 400,
+            status : false,
+            message : "Validation Error",
+            error : errors.array(),
+            data : []
+        })
+    }
+
+
+});
+
+app.get('/student',function(req,res){
+
+});
+
+app.get('/student/:student_id',function(req,res){
+
+});
+
+app.put('/student/:student_id',ValidationRules,function(req,res){
+
+});
+
+app.delete('/student/:student_id',function(req,res){
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
 app.listen(PORT,function(){
     console.log('Server Started at PORT = '+PORT);
-    connect();
+    // connect();
 });
 
 
